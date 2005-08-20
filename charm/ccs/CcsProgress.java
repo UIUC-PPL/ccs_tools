e5cb4983bad47f160783561031ff23b682fa9ee9
Isaac Dooley <idooley2@illinois.edu>
2005-08-20 18:09:43 +0000

    Added to make the lvClient be a little simpler.

---
+++ charm/ccs/CcsProgress.java
@@ -0,0 +1,14 @@
+/*
+  
+A simple progress class. Was created due to some problems making an java.awt.Label into a progress.
+
+*/
+
+package charm.ccs;
+
+import java.awt.Label;
+
+
+public interface CcsProgress {
+  public void setText(String s);
+};
