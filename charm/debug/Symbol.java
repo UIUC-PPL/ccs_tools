74d46f5102e26aae4bca8d015c0edbcc97fdcc1f
Filippo Gioachin <gioachin@illinois.edu>
2007-11-21 00:37:58 +0000

    Added equal operator, changed definition of gdb info command (no prefix needs to be appended)

--- charm/debug/Symbol.java
+++ charm/debug/Symbol.java
@@ -5,31 +5,41 @@ import java.util.Hashtable;
 public class Symbol {
     public static Hashtable symbolTable = new Hashtable();
 
+    private long location;
     private String function;
     private int line;
     private String file;
 
     public String toString() {
-	return function+" at "+file+":"+line;
+    	return function+" (0x"+Long.toHexString(location)+")"+" at "+file+":"+line;
     }
 
-    public Symbol(String str, int l, String f) {
-	function = str;
-	line = l;
-	file = f;
+    public Symbol(long loc, String str, int l, String f) {
+    	location = loc;
+    	function = str;
+    	line = l;
+    	file = f;
     }
 
     public String getFunction() {return function;}
 
+    public boolean equals(Symbol o) {
+    	return location == o.location;
+    }
+    
+    public boolean equals(long loc) {
+    	return location == loc;
+    }
+    
     public static Symbol get(long location) {
     	Symbol s = (Symbol)symbolTable.get(new Long(location));
     	if (s == null) {
     		// resolve the symbol in the info gdb
-    		String res1 = ParDebug.infoCommand("info:info symbol "+location+"\n");
+    		String res1 = ParDebug.infoCommand("info symbol "+location+"\n");
     		//System.out.println(res1);
     		int index = res1.indexOf('+');
     		String funcName = index>=0 ? res1.substring(0, index).trim() : "??";
-    		String res2 = ParDebug.infoCommand("info:info line *"+location+"\n");
+    		String res2 = ParDebug.infoCommand("info line *"+location+"\n");
     		index = res2.indexOf("Line");
     		String fileName;
     		int line;
@@ -44,7 +54,7 @@ public class Symbol {
     			index2 = res2.indexOf('"', index+1);
     			fileName = res2.substring(index+1,index2).trim();
     		}
-    		s = new Symbol(funcName, line, fileName);
+    		s = new Symbol(location, funcName, line, fileName);
     		Symbol.put(location, s);
     	}
     	return s;
