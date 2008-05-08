3741f91f61a519aa17bddf0c86ab3d47c20ea3bb
Filippo Gioachin <gioachin@illinois.edu>
2008-05-08 01:08:54 +0000

    - moved configuration and Executions to XML files

--- charm/debug/preference/Preference.java
+++ charm/debug/preference/Preference.java
@@ -1,8 +1,11 @@
 package charm.debug.preference;
 
+import charm.util.ReflectiveXML;
+
 import java.io.*;
 import java.awt.*;
 import javax.swing.*;
+import org.xml.sax.SAXException;
 
 public class Preference extends JPanel {
 	public Dimension size;
@@ -20,18 +23,21 @@ public class Preference extends JPanel {
 	}
 	public void addRecent(String file) {
 		recentConfig.add(file);
+		//System.out.println("Adding recent file: "+file);
 	}
 	
-	public void load() {
-		BufferedReader in;
+	public static Preference load() {
+		FileReader in;
 		String str;
 		try {
-			in = new BufferedReader(new FileReader(System.getProperty("user.home")+"/.cpdrc"));
+			in = new FileReader(System.getProperty("user.home")+"/.cpdrc");
 		} catch (FileNotFoundException e) {
-			return;
+			return null;
 		}
 		System.out.println("Loading preference file "+System.getProperty("user.home")+"/.cpdrc");
 		try {
+			Preference pref = (Preference)ReflectiveXML.read(in);
+			/*
 			str = in.readLine();
 			str = str.substring(str.indexOf(' ')+1);
 			try {
@@ -45,20 +51,26 @@ public class Preference extends JPanel {
 						Integer.parseInt(str.substring(str.indexOf(' ')+1)));
 			} catch (Exception e) { }
 			recentConfig.load(in);
+			*/
 			in.close();
+			return pref;
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(this, "Could not load configuration file.\nPlease delete the file '.cpdrc' from your home directory.", "Failed to load configuration", JOptionPane.ERROR_MESSAGE);
-			return;
+			JOptionPane.showMessageDialog(null, "Could not load configuration file.\nPlease delete the file '.cpdrc' from your home directory.", "Failed to load configuration", JOptionPane.ERROR_MESSAGE);
+			return null;
+		} catch (SAXException e) {
+			JOptionPane.showMessageDialog(null, "Could not load configuration file.\nPlease delete the file '.cpdrc' from your home directory.", "Failed to load configuration", JOptionPane.ERROR_MESSAGE);
+			return null;
 		}
 	}
 	
 	public void save() {
-		BufferedWriter out;
+		FileWriter out;
 		try {
-			out = new BufferedWriter(new FileWriter(System.getProperty("user.home")+"/.cpdrc"));
-			out.write("WindowSize "+(size!=null?size.width+" "+size.height:"?")+"\n");
-			out.write("WindowPosition "+(location!=null?location.x+" "+location.y:"?")+"\n");
-			recentConfig.save(out);
+			out = new FileWriter(System.getProperty("user.home")+"/.cpdrc");
+			ReflectiveXML.write(out, this);
+			//out.write("WindowSize "+(size!=null?size.width+" "+size.height:"?")+"\n");
+			//out.write("WindowPosition "+(location!=null?location.x+" "+location.y:"?")+"\n");
+			//recentConfig.save(out);
 			out.close();
 		} catch (IOException e) {
 			System.out.println("Failed to save configuration file");
