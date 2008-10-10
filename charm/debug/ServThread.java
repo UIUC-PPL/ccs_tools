99ca4bd3ee548a4632b4959b17a7a667cfde58af
Filippo Gioachin <gioachin@illinois.edu>
2008-10-10 04:25:38 +0000

    Splitted ServThread into a multipurpose server. Before it was capable only of reading input from a process (namely charmrun); now it can read input from various sources: charmrun process, charmrun through CCS, output file. This allows the attach to a running application, and still be able to receive its output for processing. (as of now, some information like breakpoints reached come out of the parallel application as prints)

--- charm/debug/ServThread.java
+++ charm/debug/ServThread.java
@@ -4,6 +4,9 @@ package charm.debug;
 import java.io.*;
 
 import javax.swing.SwingUtilities;
+
+import charm.ccs.CcsServer;
+import charm.debug.preference.Execution;
 //import java.security.*;
 
 
@@ -12,16 +15,179 @@ import javax.swing.SwingUtilities;
  * with the currently debugged program. The communication here refers to the
  * stdin, stdout, stderr streams.
  */
-public class ServThread extends Thread {
+public abstract class ServThread extends Thread {
 	String hostName = null;
 	String portno = null;
-	Runtime runtime = null; 
-	Process p = null; 
+	//Runtime runtime = null; 
 	ParDebug mainThread = null;
 	volatile int flag;
-	public ServThread(ParDebug d, Process par){
+	static final int maxChunk=50*1024; /* maximum chunk size */
+	
+	public static class Charmrun extends ServThread {
+		Process p = null; 
+		BufferedReader prout, prerr;
+
+		public Charmrun(ParDebug d, Process par) {
+			super(d);
+			p = par;
+			prout = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			prerr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+		}
+
+		String getNextOutput(StringBuffer outlinechunk) throws Exception {
+			boolean foundPort = false;
+			String outline;
+			
+			do
+			{ /* Fetch a line of output and handle it */
+				outline = prout.readLine();
+				if (outline == null) break;
+				if(!foundPort)
+				{
+					int portStart, portEnd;
+					int nameStart, nameEnd;
+					//System.out.println("Debug servhread: "+outline);
+					if(outline.indexOf("ccs: Server IP =", 0) != -1)
+					{
+						nameStart = outline.indexOf("Server IP = ",0);
+						nameStart += 12;
+						nameEnd = outline.indexOf(",",nameStart);
+						hostName = outline.substring(nameStart, nameEnd);
+						portStart = outline.indexOf("Server port = ",0);
+						portStart += 14;
+						portEnd = outline.indexOf("$",0);
+						portno = outline.substring(portStart, portEnd-1);
+						foundPort = true;
+						flag = 1;
+					}
+					if(outline.indexOf("Password:", 0) != -1) {
+						System.out.println("Password requested");
+						char [] passwd = null;
+						new PasswordDialog(passwd);
+						System.out.println(passwd);
+					}
+				}
+				//else {
+				// User output: Print this out to a display area on the debugger
+				// Flush out stderr too:
+
+				// CHANGE Filippo: In order to use a stream for the debug,
+				// everything is printed by charmrun to stdout. stderr is now
+				// used for debugging (anyway they were treated in the same
+				// way).
+
+				//while (prerr.ready()) outline+=prerr.readLine()+"\n";
+				outlinechunk.append(outline+"\n");
+				//}
+
+			}
+			while (prout.ready() && (outlinechunk.length()<maxChunk));
+			return outline;
+		}
+	}
+	
+	public static class CCS extends ServThread {
+		CcsServer server;
+		public CCS(ParDebug d, Execution e) {
+			super(d);
+			String[] args = new String[2];
+			args[0] = e.hostname;
+			args[1] = e.port;
+			server = CcsServer.create(args, false);
+			System.out.println("Created CCSserver");
+			try {
+	            server.sendRequest("redirect stdio", 0);
+            } catch (IOException e1) {
+            	System.out.println("redirect stdio request failed");
+            }
+		}
+		
+		String getNextOutput(StringBuffer outlinechunk) {
+			CcsServer.Request r;
+            try {
+	            r = server.sendRequest("fetch stdio", 0);
+				byte[] resp=server.recvResponse(r);
+				System.out.println("reply fetch stdio: "+new String(resp));
+				if (resp != null) {
+					outlinechunk.append(new String(resp));
+					return new String(resp);
+				}
+				else return null;
+            } catch (IOException e) {
+            	System.out.println("Exception while fetching stdio");
+            	e.printStackTrace();
+	            return null;
+            }
+			
+		}
+	}
+	
+	public static class File extends ServThread {
+		BufferedReader prout;
+
+		public File(ParDebug d, java.io.File f) {
+			super(d);
+			try {
+				prout = new BufferedReader(new FileReader(f));
+			} catch (FileNotFoundException e) {
+				System.out.println("Could not open file "+f);
+			}
+		}
+
+		String getNextOutput(StringBuffer outlinechunk) throws Exception {
+			boolean foundPort = false;
+			String outline;
+			
+			do
+			{ /* Fetch a line of output and handle it */
+				outline = prout.readLine();
+				if (outline == null) break;
+				if(!foundPort)
+				{
+					int portStart, portEnd;
+					int nameStart, nameEnd;
+					//System.out.println("Debug servhread: "+outline);
+					if(outline.indexOf("ccs: Server IP =", 0) != -1)
+					{
+						nameStart = outline.indexOf("Server IP = ",0);
+						nameStart += 12;
+						nameEnd = outline.indexOf(",",nameStart);
+						hostName = outline.substring(nameStart, nameEnd);
+						portStart = outline.indexOf("Server port = ",0);
+						portStart += 14;
+						portEnd = outline.indexOf("$",0);
+						portno = outline.substring(portStart, portEnd-1);
+						foundPort = true;
+						flag = 1;
+					}
+					if(outline.indexOf("Password:", 0) != -1) {
+						System.out.println("Password requested");
+						char [] passwd = null;
+						new PasswordDialog(passwd);
+						System.out.println(passwd);
+					}
+				}
+				//else {
+				// User output: Print this out to a display area on the debugger
+				// Flush out stderr too:
+
+				// CHANGE Filippo: In order to use a stream for the debug,
+				// everything is printed by charmrun to stdout. stderr is now
+				// used for debugging (anyway they were treated in the same
+				// way).
+
+				//while (prerr.ready()) outline+=prerr.readLine()+"\n";
+				outlinechunk.append(outline+"\n");
+				//}
+
+			}
+			while (prout.ready() && (outlinechunk.length()<maxChunk));
+			return outline;
+		}
+	}
+	
+	public ServThread(ParDebug d) {
 		mainThread = d;
-		p = par;
 		flag = 0;
 		/*try {
 			//charmrunIn = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
@@ -39,100 +205,27 @@ public class ServThread extends Thread {
 	// string used to pass the output back from the info gdb
 	public static String infoStr;
 
-
+	/* This method is specific for each type of input method:
+	 * STDOUT for programs started directly;
+	 * CCS for programs attached.
+	 */
+	abstract String getNextOutput(StringBuffer outlinechunk) throws Exception;
+	
 	public void run() {
-		runtime = Runtime.getRuntime();
+		//runtime = Runtime.getRuntime();
 		try {
 			System.out.println("ServThread started");
-			BufferedReader prout = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			BufferedReader prerr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-			boolean foundPort = false;
 			try {
 				while (true) 
 				{ /* Fetch a chunk of output and print it.
 				 * Passing the output to the GUI in large chunks is 
 				 * much much faster than passing it one line at a time.
 				 */
-					String outline;
 					StringBuffer outlinechunk=new StringBuffer();
-					final int maxChunk=50*1024; /* maximum chunk size */
-					do
-					{ /* Fetch a line of output and handle it */
-						outline = prout.readLine();
-						if (outline == null) break;
-						if(!foundPort)
-						{
-							int portStart, portEnd;
-							int nameStart, nameEnd;
-							//System.out.println("Debug servhread: "+outline);
-							if(outline.indexOf("ccs: Server IP =", 0) != -1)
-							{
-								nameStart = outline.indexOf("Server IP = ",0);
-								nameStart += 12;
-								nameEnd = outline.indexOf(",",nameStart);
-								hostName = outline.substring(nameStart, nameEnd);
-								portStart = outline.indexOf("Server port = ",0);
-								portStart += 14;
-								portEnd = outline.indexOf("$",0);
-								portno = outline.substring(portStart, portEnd-1);
-								foundPort = true;
-								flag = 1;
-							}
-							if(outline.indexOf("Password:", 0) != -1) {
-								System.out.println("Password requested");
-								char [] passwd = null;
-								new PasswordDialog(passwd);
-								System.out.println(passwd);
-							}
-						}
-
-						if (outline.indexOf("Break point reached", 0) != -1)
-						{
-							mainThread.setStatusMessage(outline);
-							Runnable doWorkRunnable = new Notify(outline) {
-							    public void run() { mainThread.notifyBreakpoint(text); }
-							};
-							SwingUtilities.invokeLater(doWorkRunnable);
-							//mainThread.notifyBreakpoint(outline);
-						}
-						else if (outline.indexOf("Frozen processor") != -1) {
-							mainThread.setStatusMessage(outline);
-							Runnable doWorkRunnable = new Notify(outline) {
-								public void run() { mainThread.notifyFreeze(text); }
-							};
-							SwingUtilities.invokeLater(doWorkRunnable);
-						}
-						else if (outline.indexOf("CPD: CmiAbort called") != -1) {
-							mainThread.setStatusMessage(outline.substring(5));
-							Runnable doWorkRunnable = new Notify(outline) {
-								public void run() { mainThread.notifyAbort(text); }
-							};
-							SwingUtilities.invokeLater(doWorkRunnable);
-						}
-						else if (outline.indexOf("CPD: Signal received") != -1) {
-							mainThread.setStatusMessage(outline.substring(5));
-							Runnable doWorkRunnable = new Notify(outline) {
-								public void run() { mainThread.notifySignal(text); }
-							};
-							SwingUtilities.invokeLater(doWorkRunnable);
-						}
-						else {
-							// User output: Print this out to a display area on the debugger
-							// Flush out stderr too:
-
-							// CHANGE Filippo: In order to use a stream for the debug,
-							// everything is printed by charmrun to stdout. stderr is now
-							// used for debugging (anyway they were treated in the same
-							// way).
-
-							//while (prerr.ready()) outline+=prerr.readLine()+"\n";
-							outlinechunk.append(outline+"\n");
-						}
-					}
-					while (prout.ready() && (outlinechunk.length()<maxChunk));
-					System.out.println("Parallel program printed: "+outlinechunk.toString());
-					mainThread.displayProgramOutput(outlinechunk.toString());
-
+					String outline = getNextOutput(outlinechunk);
+					
+					process(outlinechunk);
+					
 					if (outline==null) {
 						System.out.println("ServThread terminated");
 						flag = 2;
@@ -157,6 +250,58 @@ public class ServThread extends Thread {
 		} 
 	}
 	
+	void process(StringBuffer outlinechunk) {
+		int cpdStart;
+		while ((cpdStart = outlinechunk.indexOf("CPD: ")) != -1) {
+			int cpdEnd = outlinechunk.indexOf("\n", cpdStart+1);
+			String outline = outlinechunk.substring(cpdStart+5, cpdEnd);
+			outlinechunk.delete(cpdStart, cpdEnd+1);
+			if (outline.indexOf("Break point reached", 0) != -1)
+			{
+				mainThread.setStatusMessage(outline);
+				Runnable doWorkRunnable = new Notify(outline) {
+					public void run() { mainThread.notifyBreakpoint(text); }
+				};
+				SwingUtilities.invokeLater(doWorkRunnable);
+				//mainThread.notifyBreakpoint(outline);
+			}
+			else if (outline.indexOf("Frozen processor") != -1) {
+				mainThread.setStatusMessage(outline);
+				Runnable doWorkRunnable = new Notify(outline) {
+					public void run() { mainThread.notifyFreeze(text); }
+				};
+				SwingUtilities.invokeLater(doWorkRunnable);
+			}
+			else if (outline.indexOf("CmiAbort called") != -1) {
+				mainThread.setStatusMessage(outline.substring(5));
+				Runnable doWorkRunnable = new Notify(outline) {
+					public void run() { mainThread.notifyAbort(text); }
+				};
+				SwingUtilities.invokeLater(doWorkRunnable);
+			}
+			else if (outline.indexOf("Signal received") != -1) {
+				mainThread.setStatusMessage(outline.substring(5));
+				Runnable doWorkRunnable = new Notify(outline) {
+					public void run() { mainThread.notifySignal(text); }
+				};
+				SwingUtilities.invokeLater(doWorkRunnable);
+			} else {
+				System.out.println("ServThread: error while processing string '"+outline+"'");
+			}
+		}
+		/*else if (outline.indexOf("CPD: Object") != -1) {
+		mainThread.setStatusMessage(outline.substring(5));
+		Runnable doWorkRunnable = new Notify(outline) {
+			public void run() { mainThread.notifyObjectViolation(text); }
+		};
+		SwingUtilities.invokeLater(doWorkRunnable);
+	}*/
+		if (outlinechunk.length() > 0) {
+			System.out.println("Parallel program printed: "+outlinechunk.toString());
+			mainThread.displayProgramOutput(outlinechunk.toString());
+		}
+	}
+
 	public int getFlag() { return flag; }
 	
 	public abstract class Notify implements Runnable {
