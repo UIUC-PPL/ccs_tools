c9d92df33082bf68d378af362ca6ebc85c1d21e5
Zack Berman <zberman2@illinois.edu>
2014-06-03 14:18:47 -0500

    Flags added to the command line arguments such that the program is run like this: ./liveViz localhost 1234 -w <width> -h <height>
    Change-Id: I303f98daf6c6992d0877e11aaaa14a82aaf623a8

--- charm/liveViz/MainApplet.java
+++ charm/liveViz/MainApplet.java
@@ -10,49 +10,78 @@ import java.awt.event.*;
 
 public class MainApplet extends Applet
 {
-//This little stub main allows the applet to also be run
+// This little stub main allows the applet to also be run
 // as a java application.
 	public static void main(String args[]) {
-		if (args.length!=2) {
-			System.out.println("Usage: demo <ccs server> <ccs port>\n");
+		if (args.length<2) {
+			System.out.println("Usage: demo <ccs server>" +
+			  "<ccs port>\nor:    demo <ccs server> <ccs port>" +
+			  " -w <width> -h <height>");
 			System.exit(1);
 		}
-		else {
-		    createWindow(new MainPanel(args[0],args[1]),true);
+
+		int     width     = 300;
+		int     height    = 200;
+		boolean maximized = false;
+
+		boolean wFlag = false;
+		boolean hFlag = false;
+
+		for (int i = 2; i < args.length; i++) {
+			if (args[i].equals("-w")) {
+				wFlag = true;
+			} else if (args[i].equals("-h")) {
+				hFlag = true;
+			} else if (wFlag) {
+				width = Integer.parseInt(args[i]);
+				wFlag = false;
+			} else if (hFlag) {
+				height = Integer.parseInt(args[i]);
+				hFlag = false;
+			}
 		}
+		if (width < 0 || height < 0) {
+			System.out.println("ERROR: Invalid width or height");
+			System.exit(1);
+		}
+
+		createWindow(new MainPanel(args[0],args[1]),true, width,
+		height, maximized);
 	}
 
-	private static Frame createWindow(MainPanel p,boolean closeExit)
-	{
+	private static Frame createWindow(MainPanel p,boolean closeExit,
+	  int width, int height, boolean maximized) {
 		Frame top=new Frame();
-		if (closeExit)
+		if (maximized) top.setExtendedState(Frame.MAXIMIZED_BOTH);
+		if (closeExit) {
 		  top.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e)
 			{ System.exit(0); }
 		  });
+		}
 		top.setTitle("Parallel Data Visualization");
 		top.add(p);
 		top.pack();
-		top.setSize(300,200);
+		top.setSize(width,height);
 		top.setVisible(true);
 		return top;
 	}
 
-	
-//Properties of the applet proper:
-//  show/hide window when requested.	
+// Properties of the applet proper:
+// show/hide window when requested.
 	private MainPanel panel;
 	private Frame frame;
-	public void start()
-	{
+
+	public void start() {
 		panel=new MainPanel(getParameter("ccs_server_name"),
-			   getParameter("ccs_server_port"));
-		frame=createWindow(panel,false);
+			getParameter("ccs_server_port"));
+		frame=createWindow(panel,false, 300, 200, false);
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e)
 			{ e.getWindow().dispose(); }
 		});
 	}
+
 	public void stop() {
 		panel.stop();
 		frame.dispose();
--- charm/liveViz/README.txt
+++ charm/liveViz/README.txt
@@ -3,6 +3,9 @@ Tiny liveViz (-module liveViz) client program.
 Run it like:
 	charm/bin/liveViz localhost 1234
 
+or, to specify a width and height of the liveViz window:
+	charm/bin/liveViz localhost 1234 -w 1000 -h 1000
+
 An example server for this client is in 
 	charm/pgms/charm++/ccs/liveViz/server/
 
